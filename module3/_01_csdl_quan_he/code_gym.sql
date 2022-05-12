201b252bed8dd2b0f97ad49eb06a0c4e4bce6b4b
C0222G1-NguyenHoangHao <hoanghao9x87@gmail.com>
2022-05-12 11:30:09 +0700

    update

---
+++ module3/_01_csdl_quan_he/code_gym.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE code_gym;
+
+use code_gym;
+
+CREATE TABLE class(
+	id INT NOT NULL PRIMARY KEY,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE teacher(
+	id INT NOT NULL PRIMARY KEY,
+    `name` VARCHAR(50) NOT NULL,
+    age int,
+    country VARCHAR(50)
+);
+
+SELECT * FROM code_gym.class;
+SELECT * FROM code_gym.teateachercher;
