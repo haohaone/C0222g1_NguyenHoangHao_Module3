5d8ae3b591b0a37ec0c874c14459dc90acec4b28
C0222G1-NguyenHoangHao <hoanghao9x87@gmail.com>
2022-05-18 13:52:49 +0700

    update

---
+++ module3/_05_view_procedure_index/exercise/luyen_tap_view_procedure_index.sql
@@ -0,0 +1,82 @@
+CREATE DATABASE products;
+
+USE products;
+
+CREATE TABLE products(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    product_code VARCHAR(50),
+    product_name VARCHAR(50),
+    product_price INT,
+    product_amonut INT,
+    product_decription VARCHAR(100),
+    product_status VARCHAR(50)
+);
+
+INSERT INTO products VALUE
+(1, 'IP-X', 'iphone X', 500, 10, 'From USA', '99%'),
+(2, 'IP-11', 'iphone 11', 700, 10, 'From USA', '100%'),
+(3, 'IP-13', 'iphone 13', 1500, 5, 'From UK', '99%'),
+(4, 'SS13', 'Sam Sung 13', 200, 4, 'From Korea', '100%'),
+(5, 'HW-S', 'Huawei S', 1300, 9, 'From Taiwan', '95%');
+
+EXPLAIN SELECT * FROM products WHERE product_code = 'HW-S';
+
+CREATE INDEX id_products_code ON products(product_code);
+
+EXPLAIN SELECT * FROM products WHERE product_code = 'HW-S';
+
+CREATE INDEX composite_id_products ON products(product_decription, product_status);
+
+EXPLAIN SELECT * FROM products WHERE product_decription = 'From USA' AND product_status = '100%';
+
+CREATE VIEW view_test AS
+SELECT product_code, product_name, product_price, product_status FROM products;
+
+SELECT * FROM view_test;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE view_test
+SET product_status = '100%';
+
+DELETE FROM view_test
+WHERE product_price < 700;
+
+DELIMITER \\
+CREATE PROCEDURE get_all_products()
+BEGIN
+	SELECT * FROM products;
+END\\
+DELIMITER ;
+
+CALL get_all_products;
+
+DELIMITER \\
+CREATE PROCEDURE add_new_products()
+BEGIN
+	INSERT INTO products (product_code, product_name, product_price, product_amonut, product_decription, product_status)
+    VALUE ('IP-15-PRO', 'Iphone 15', 4000, 2, 'From VN', '100%');
+END\\
+DELIMITER ;
+
+CALL add_new_products;
+
+DELIMITER \\
+CREATE PROCEDURE edit_price_by_id(IN set_id INT, IN price INT)
+BEGIN
+	UPDATE products
+    SET product_price = price
+    WHERE id = set_id;
+END\\
+DELIMITER ;
+
+CALL edit_price_by_id(3, 2000)
+
+DELIMITER \\
+CREATE PROCEDURE delete_product_by_id(IN set_id INT)
+BEGIN
+	DELETE FROM products
+    WHERE id = set_id;
+END\\
+DELIMITER ;
+
+CALL delete_product_by_id(3)
--- module3/case_study_1/truy_van_furama.sql
+++ module3/case_study_1/truy_van_furama.sql
@@ -101,6 +101,92 @@ GROUP BY dvdk.ma_dich_vu_dinh_kem;
 -- hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc
 -- “Quảng Ngãi”.
 
-SELECT kh.ma_khach_hang, lk.ma_loai_khach, kh.ho_ten, kh.ngay_sinh, kh.dia_chi FROM khach_hang kh
-JOIN loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach
-WHERE kh.dia_chi = 'Quảng Nam' OR kh.dia_chi = 'Quảng Trị'
+SELECT dvdk.ma_dich_vu_dinh_kem, dvdk.ten_dich_vu_dinh_kem, dvdk.gia, dvdk.don_vi, dvdk.trang_thai, kh.ho_ten, lk.loai_khach_hang, kh.dia_chi FROM dich_vu_dinh_kem dvdk
+JOIN hop_dong_chi_tiet hdct ON hdct.ma_dich_vu_dinh_kem = dvdk.ma_dich_vu_dinh_kem
+JOIN hop_dong hd ON hd.ma_hop_dong = hdct.ma_hop_dong
+JOIN dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
+JOIN khach_hang kh ON kh.ma_khach_hang = hd.ma_khach_hang
+JOIN loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach 
+WHERE lk.loai_khach_hang = 'Diamonmd' AND (kh.dia_chi = 'Đà Nẵng' OR kh.dia_chi = 'Quảng Trị');
+-- uyenngu.com
+
+-- 12.Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách
+-- hàng), so_dien_thoai (khách hàng), ten_dich_vu,
+-- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở
+-- dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ đã từng được
+-- khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được
+-- khách hàng đặt vào 6 tháng đầu năm 2021.
+
+SELECT hd.ma_hop_dong, nv.ho_ten AS ho_vien_nhan_vien, kh.ho_ten AS ho_ten_khach_hang, kh.so_dien_thoai, dv.ten_dich_vu, count(hdct.ma_dich_vu_dinh_kem) AS so_luong_dvdk, hd.tien_dat_coc
+FROM hop_dong_chi_tiet hdct 
+JOIN dich_vu_dinh_kem AS dvdk ON dvdk.ma_dich_vu_dinh_kem = hdct.ma_dich_vu_dinh_kem
+JOIN hop_dong AS hd ON hd.ma_hop_dong = hdct.ma_hop_dong
+JOIN dich_vu AS dv ON dv.ma_dich_vu = hd.ma_hop_dong
+JOIN khach_hang AS kh ON kh.ma_khach_hang = hd.ma_khach_hang
+JOIN nhan_vien AS nv ON nv.ma_nhan_vien = hd.ma_nhan_vien
+WHERE hd.ma_dich_vu NOT IN (SELECT ma_dich_vu FROM hop_dong WHERE year(ngay_lam_hop_dong) = 2020 AND quarter(ngay_lam_hop_dong) = 4)
+AND hd.ma_dich_vu NOT IN (SELECT ma_dich_vu FROM hop_dong WHERE year(ngay_lam_hop_dong) = 2021 AND quarter(ngay_lam_hop_dong) in (1,2))
+GROUP BY hdct.ma_dich_vu_dinh_kem;
+
+-- 13. Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các
+-- Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử
+-- dụng nhiều như nhau).
+
+SELECT dvdk.ma_dich_vu_dinh_kem, dvdk.ten_dich_vu_dinh_kem, dvdk.gia, count(dvdk.ma_dich_vu_dinh_kem)AS so_luong_su_dung_nhieu_nhat, hd.ma_hop_dong 
+FROM hop_dong_chi_tiet hdct 
+JOIN dich_vu_dinh_kem AS dvdk ON dvdk.ma_dich_vu_dinh_kem = hdct.ma_dich_vu_dinh_kem
+JOIN hop_dong AS hd ON hd.ma_hop_dong = hdct.ma_hop_dong
+GROUP BY dvdk.ma_dich_vu_dinh_kem
+HAVING so_luong_su_dung_nhieu_nhat = (SELECT count(ma_dich_vu_dinh_kem) AS so_luong FROM hop_dong_chi_tiet GROUP BY ma_dich_vu_dinh_kem ORDER BY so_luong LIMIT 1);
+
+-- 14. Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một
+-- lần duy nhất. Thông tin hiển thị bao gồm ma_hop_dong,
+-- ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính
+-- dựa trên việc count các ma_dich_vu_di_kem).
+
+SELECT hd.ma_hop_dong, ldv.ten_loai_dich_vu, dvdk.ten_dich_vu_dinh_kem, dvdk.gia, count(dvdk.ma_dich_vu_dinh_kem)AS so_luong_su_dung_nhieu_nhat 
+FROM hop_dong_chi_tiet hdct 
+JOIN dich_vu_dinh_kem AS dvdk ON dvdk.ma_dich_vu_dinh_kem = hdct.ma_dich_vu_dinh_kem
+JOIN hop_dong AS hd ON hd.ma_hop_dong = hdct.ma_hop_dong
+JOIN dich_vu AS dv ON dv.ma_dich_vu = hd.ma_hop_dong
+JOIN loai_dich_vu AS ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+GROUP BY dvdk.ma_dich_vu_dinh_kem HAVING count(dvdk.ma_dich_vu_dinh_kem) = 1;
+
+-- 15. Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten,
+-- ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi mới chỉ lập được
+-- tối đa 3 hợp đồng từ năm 2020 đến 2021.
+
+SELECT nv.ma_nhan_vien, nv.ho_ten, td.ten_trinh_do, bp.ten_bo_phan, nv.so_dien_thoai, nv.dia_chi, count(nv.ma_nhan_vien) AS so_lan_lap_hop_dong FROM nhan_vien nv
+JOIN hop_dong hd ON hd.ma_nhan_vien = nv.ma_nhan_vien
+JOIN trinh_do td ON td.ma_trinh_do = nv.ma_trinh_do
+JOIN bo_phan bp ON bp.ma_bo_phan = nv.ma_bo_phan
+GROUP BY hd.ma_nhan_vien 
+HAVING so_lan_lap_hop_dong <= 3 AND nv.ma_nhan_vien IN (SELECT ma_nhan_vien FROM hop_dong WHERE year(ngay_lam_hop_dong) IN (2020, 2021));
+
+-- 16. Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019
+-- đến năm 2021. 
+
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM nhan_vien 
+WHERE nhan_vien.ma_nhan_vien NOT IN  (SELECT ma_nhan_vien FROM hop_dong WHERE year(ngay_lam_hop_dong) BETWEEN 2019 AND 2021 );
+
+-- 17. Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum
+-- lên Diamond, chỉ cập nhật những khách hàng đã từng đặt phòng với
+-- Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ
+
+UPDATE khach_hang 
+SET ma_loai_khach = 1 
+WHERE ma_loai_khach = 2 
+AND ma_khach_hang IN 
+(SELECT kh.ma_khach_hang FROM hop_dong  AS hd
+ JOIN hop_dong_chi_tiet AS hdct ON hdct.ma_hop_dong = hd.ma_hop_dong
+ JOIN dich_vu_dinh_kem AS dvdk ON dvdk.ma_dich_vu_dinh_kem = hdct.ma_dich_vu_dinh_kem
+ JOIN dich_vu AS dv ON dv.ma_dich_vu = hd.ma_dich_vu
+ JOIN khach_hang AS kh ON kh.ma_khach_hang = hd.ma_hop_dong
+ WHERE dv.chi_phi_thue + (hdct.so_luong * dvdk.gia) > 3500);
+ 
+-- 18.Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc
+-- giữa các bảng).
+ 
+DELETE FROM khach_hang 
+WHERE khach_hang.ma_khach_hang NOT IN (SELECT ma_khach_hang FROM hop_dong WHERE year(ngay_lam_hop_dong) = 2021);
